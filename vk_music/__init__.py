d44586f5fae0f62da8d32806cd7392b05ff0d2ab
STLEON <leonst998@gmail.com>
2016-04-03 22:31:44 +0300

    name

--- vk_music/__init__.py
+++ vk_music/__init__.py
@@ -1,7 +1,7 @@
 __title__ = 'vk_music'
 __version__ = '0.0.1'
-__author__ = 'ST LEON'
+__author__ = 'Lev Tonkikh'
 __license__ = ''
-__copyright__ = 'Copyright 2016 ST LEON'
+__copyright__ = 'Copyright 2016 Lev Tonkikh'
 
 from .vk_music import Track, VkMusic
